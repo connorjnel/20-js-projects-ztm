1c948f6bd9836652fae379a08b4231e8be5528d7
connorjnel <connor.j.nel00@gmail.com>
2022-10-19 10:23:56 +0100

    quote generator

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -2,11 +2,25 @@ const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const quoteAuthor = document.getElementById("author");
 const quoteButton = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Show loading spinner
+function showLoadingSpinner() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+// Hide loading spinner
+function hideLoadingSpinner() {
+	quoteContainer.hidden = false;
+	loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+	showLoadingSpinner();
 	// Pick a random quote from apiQuotes array
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -23,7 +37,9 @@ function newQuote() {
 	} else {
 		quoteText.classList.remove("long-quote");
 	}
+	// Set quote, hide loader
 	quoteText.textContent = quote.text;
+	hideLoadingSpinner();
 }
 
 // Event listener quote button - call new quote
@@ -31,6 +47,7 @@ quoteButton.addEventListener("click", newQuote);
 
 // Get quotes from API
 async function getQuotes() {
+	showLoadingSpinner();
 	// Quote source
 	const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
 
