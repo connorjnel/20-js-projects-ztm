ee60aaff8fa67735ba29976895bdd0614d5b5623
connorjnel <connor.j.nel00@gmail.com>
2022-10-24 10:54:40 +0100

    joke teller

--- joke-teller/script.js
+++ joke-teller/script.js
@@ -1,48 +1,39 @@
-const button = document.getElementById('button');
-const audioElement = document.getElementById('audio');
+const voiceAPI = "a691cf59f4ee46e0bc64e0c8d648fb52";
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
 
-// Disable/Enable Button
-function toggleButton() {
-  button.disabled = !button.disabled;
-}
+// function test() {
+// 	VoiceRSS.speech({
+// 		key: voiceAPI,
+// 		src: "Connor",
+// 		hl: "en-us",
+// 		v: "John",
+// 		r: 0,
+// 		c: "mp3",
+// 		f: "44khz_16bit_stereo",
+// 		ssml: false,
+// 	});
+// }
+// test();
 
-// VoiceRSS Speech Function
-function tellMe(joke) {
-  const jokeString = joke.trim().replace(/ /g, '%20');
-  // VoiceRSS Speech Parameters
-  VoiceRSS.speech({
-    key: 'YOUR_API_KEY_HERE',
-    src: jokeString,
-    hl: 'en-us',
-    r: 0,
-    c: 'mp3',
-    f: '44khz_16bit_stereo',
-    ssml: false,
-  });
-}
-
-// Get jokes from Joke API
+// Get jokes from JokeAPI
 async function getJokes() {
-  let joke = '';
-  const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,racist,sexist';
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    // Assign One or Two Part Joke
-    if (data.setup) {
-      joke = `${data.setup} ... ${data.delivery}`;
-    } else {
-      joke = data.joke;
-    }
-    // Passing Joke to VoiceRSS API
-    tellMe(joke);
-    // Disable Button
-    toggleButton();
-  } catch (error) {
-    // Catch Error Here
-  }
+	let joke = "";
+	const apiUrl = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+	try {
+		const response = await fetch(apiUrl);
+		const data = await response.json();
+		if (data.setup) {
+			joke = `${data.setup} ... ${data.delivery}`;
+		} else {
+			joke = data.joke;
+		}
+		console.log(joke);
+	} catch (error) {
+		// Catch and display error
+		console.log("You messed up: \n", error);
+	}
 }
 
-// Event Listeners
-button.addEventListener('click', getJokes);
-audioElement.addEventListener('ended', toggleButton);
+// On Load
+getJokes();
