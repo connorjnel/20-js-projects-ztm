7d62ea5f9624952c14cb54911f5e843f6182b71c
connorjnel <connor.j.nel00@gmail.com>
2022-10-25 22:27:23 +0100

    got it to work pretty well, now for course solution

--- joke-teller/script.js
+++ joke-teller/script.js
@@ -2,19 +2,18 @@ const voiceAPI = "a691cf59f4ee46e0bc64e0c8d648fb52";
 const button = document.getElementById("button");
 const audioElement = document.getElementById("audio");
 
-// function test() {
-// 	VoiceRSS.speech({
-// 		key: voiceAPI,
-// 		src: "Connor",
-// 		hl: "en-us",
-// 		v: "John",
-// 		r: 0,
-// 		c: "mp3",
-// 		f: "44khz_16bit_stereo",
-// 		ssml: false,
-// 	});
-// }
-// test();
+function tellJoke(joke) {
+	VoiceRSS.speech({
+		key: voiceAPI,
+		src: joke,
+		hl: "en-us",
+		v: "John",
+		r: 0,
+		c: "mp3",
+		f: "44khz_16bit_stereo",
+		ssml: false,
+	});
+}
 
 // Get jokes from JokeAPI
 async function getJokes() {
@@ -29,11 +28,12 @@ async function getJokes() {
 			joke = data.joke;
 		}
 		console.log(joke);
+		tellJoke(joke);
 	} catch (error) {
 		// Catch and display error
 		console.log("You messed up: \n", error);
 	}
 }
 
-// On Load
-getJokes();
+//Tell me a joke button
+button.addEventListener("click", getJokes);
