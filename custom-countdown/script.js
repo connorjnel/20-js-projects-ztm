6d992316fe682fc0f8a8bba73a877c4b33ce38d1
connorjnel <connor.j.nel00@gmail.com>
2022-11-07 20:12:26 +0000

    get form values

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -2,6 +2,20 @@ const inputContainer = document.getElementById("input-container");
 const coundtdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+let countdownTitle = "";
+let countdownDate = "";
+
 // Set date input minimum with current date
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
+
+// Take values from form input
+function updateCountdown(event) {
+	event.preventDefault();
+	countdownTitle = event.srcElement[0].value;
+	countdownDate = event.srcElement[1].value;
+	console.log(countdownTitle, countdownDate);
+}
+
+// Event Listener
+coundtdownForm.addEventListener("submit", updateCountdown);
