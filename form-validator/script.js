b1922b68a397656ec87e4cb08507304af064bfab
connorjnel <connor.j.nel00@gmail.com>
2022-11-15 23:09:29 +0000

    password validation

--- form-validator/script.js
+++ form-validator/script.js
@@ -5,14 +5,30 @@ const messageContainer = document.querySelector(".message-container");
 const message = document.getElementById("message");
 
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
 	// Using Aonstraint API
 	isValid = form.checkValidity();
 	// Style main message for an error
-	message.textContent = "Please fill out all fields";
-	message.style.color = "red";
-	messageContainer.style.borderColor = "red";
+	if (!isValid) {
+		message.textContent = "Please fill out all fields";
+		message.style.color = "red";
+		messageContainer.style.borderColor = "red";
+	}
+	// Check to see if passwords match
+	if (password1El.value === password2El.value) {
+		passwordsMatch = true;
+		password1El.style.borderColor = "green";
+		password2El.style.borderColor = "green";
+	} else {
+		passwordsMatch = false;
+		message.textContent = "Make sure passwords match.";
+		message.style.color = "red";
+		messageContainer.style.borderColor = "red";
+		password1El.style.borderColor = "red";
+		password2El.style.borderColor = "red";
+	}
 }
 
 function processFormData(event) {
