c3d504e474659f3a0ef42ddd5427bc25017506f7
connorjnel <connor.j.nel00@gmail.com>
2022-11-17 23:34:12 +0000

    reset all

--- spock-rock-game/script.js
+++ spock-rock-game/script.js
@@ -38,6 +38,18 @@ function resetSelected() {
 	});
 }
 
+// ResetScore and playerChoice / computerChoice
+function resetAll() {
+	playerScoreNumber = 0;
+	computerScoreNumber = 0;
+	playerScoreEl.textContent = playerScoreNumber;
+	computerScoreEl.textContent = computerScoreNumber;
+	playerChoiceEl.textContent = "";
+	computerChoiceEl.textContent = "";
+	resultText.textContent = "Click icon to start game";
+	resetSelected();
+}
+
 // Random Computer Choice
 function computerRandomChoice() {
 	const computerChoiceNumber = Math.random();
@@ -84,12 +96,10 @@ function displayComputerChoice() {
 
 // Check results, increase score, update resultText
 function updateScore(playerChoice) {
-	console.log(playerChoice, computerChoice);
 	if (playerChoice === computerChoice) {
 		resultText.textContent = "It's a tie.";
 	} else {
 		const choice = choices[playerChoice];
-		console.log(choice.defeats.indexOf(computerChoice));
 		if (choice.defeats.indexOf(computerChoice) > -1) {
 			resultText.textContent = "You Won!";
 			playerScoreNumber++;
@@ -139,3 +149,6 @@ function select(playerChoice) {
 			break;
 	}
 }
+
+// On startup, set initial values
+resetAll();
