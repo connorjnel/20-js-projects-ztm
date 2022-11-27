ad7f3e53cc133590a63fab9ec6c7ae6e16899ea6
connorjnel <connor.j.nel00@gmail.com>
2022-11-27 18:45:55 +0000

    nasa api fetch

--- nasa-apod/script.js
+++ nasa-apod/script.js
@@ -1,6 +1,21 @@
 // NASA API
 const count = 10;
 const apiKey = "05Y3yviP5QolU5q3aV4J1zvSOxAd2ASBnDN8odhI";
-const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
-// Still sick, ugh
+let resultsArray = [];
+
+// Get 10 images from NASA API
+async function getNasaPictures() {
+	try {
+		const response = await fetch(apiUrl);
+		resultsArray = await response.json();
+		console.log(resultsArray);
+	} catch (error) {
+		// Catch error here
+		console.log(error);
+	}
+}
+
+// On Load
+getNasaPictures();
